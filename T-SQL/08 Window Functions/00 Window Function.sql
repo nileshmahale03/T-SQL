1bfab8baf7caa09cd8bf1d3f624dadf1b5030d30
Nilesh Mahale <nileshmahale03@gmail.com>
2020-09-26 11:18:21 -0700

    started Window function, Grouping sets, PIVOT UNPIVOT
    started Window function, Grouping sets, PIVOT UNPIVOT

---
+++ T-SQL/08 Window Functions/00 Window Function.sql
@@ -0,0 +1,10 @@
+
+---------------------------------------------------------------------
+-- Window Functions
+
+-- Window functions, which you can use to apply data-analysis calculations in a flexible and efficient manner.
+
+-- A window function is a function that, for each row, computes a scalar result value based on a calculation against a subset of the rows from the underlying query.
+-- The subset of rows is known as a window
+-- The syntax for window functions uses a clause called OVER, in which you provide the window specification
+---------------------------------------------------------------------
---
+++ T-SQL/10 PIVOT UNPIVOT/00 PIVOT.sql
@@ -0,0 +1,8 @@
+
+---------------------------------------------------------------------
+-- PIVOT-UNPIVOT
+
+-- Pivoting rotates data from a state of rows to columns, and unpivoting rotates data from columns to rows,
+
+---------------------------------------------------------------------
+
