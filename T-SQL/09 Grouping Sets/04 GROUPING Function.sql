7af83d6508b73ccd1db6672508f596ec629632fd
Nilesh Mahale <nileshmahale03@gmail.com>
2020-10-10 13:07:26 -0700

    completed GROUPING Sets
    Grouping function, Grouping_ID function

--- T-SQL/09 Grouping Sets/04 GROUPING Function.sql
+++ T-SQL/09 Grouping Sets/04 GROUPING Function.sql
@@ -1,4 +1,19 @@
 
+---------------------------------------------------------------------
+-- GROUPING function
+
+-- If a grouping column allows NULLs in the table, you cannot tell for sure whether a NULL in the result set originated from the data 
+-- or is a placeholder for a nonparticipating member in a grouping set.
+
+-- One way to solve this problem is to use the GROUPING function. 
+-- This function accepts a name of a column and returns 0 if it is a member of the current grouping set (a detail element) and 1 otherwise (an aggregate element).
+
+-- For example, all rows in which GrpEmp is 0 and GrpCust is 0 are associated with the grouping set (empid, custid). 
+-- All rows in which GrpEmp is 0 and GrpCust is 1 are associated with the grouping set (empid), and so on.
+---------------------------------------------------------------------
+
+USE TSQLV4
+GO
 
 SELECT GROUPING(empid) 'GrpEmp'
      , GROUPING(custid) 'GrpCust'
--- T-SQL/09 Grouping Sets/05 GROUPING_ID Function.sql
+++ T-SQL/09 Grouping Sets/05 GROUPING_ID Function.sql
@@ -1,6 +1,31 @@
 
+---------------------------------------------------------------------
+-- GROUPING_ID function
+
+-- If a grouping column allows NULLs in the table, you cannot tell for sure whether a NULL in the result set originated from the data 
+-- or is a placeholder for a nonparticipating member in a grouping set.
+
+-- One way to solve this problem is to use the GROUPING function. 
+-- This function accepts a name of a column and returns 0 if it is a member of the current grouping set (a detail element) and 1 otherwise (an aggregate element).
+-- T-SQL supports another function, called GROUPING_ID, that can further simplify the process of associating result rows and grouping sets.
+
+-- You provide the function with all elements that participate in any grouping set as inputs—for example, GROUPING_ID(a, b, c, d)—
+-- and the function returns an integer bitmap in which each bit represents a different input element
+
+-- Now you can easily figure out which grouping set each row is associated with. 
+-- The integer 0 (binary 00) represents the grouping set (empid, custid); 
+-- the integer 1 (binary 01) represents (empid); 
+-- the integer 2 (binary 10) represents (custid); and 
+-- the integer 3 (binary 11) represents ().
+
+---------------------------------------------------------------------
+
+USE TSQLV4
+GO
 
 SELECT GROUPING_ID(empid, custid) 'GrpID'
+     , GROUPING(empid) 'GrpEmp'
+     , GROUPING(custid) 'GrpCust'
 	 , empid, custid, SUM(qty) 'SumQty'
 FROM dbo.Orders
 GROUP BY CUBE(empid, custid)
