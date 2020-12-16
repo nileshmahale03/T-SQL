a05343007c46c96386dab2e9cfce929e6deb1a86
Nilesh Mahale <nileshmahale03@gmail.com>
2020-12-15 22:38:27 -0800

    14 Data Modification - DELETE
    14 Data Modification - DELETE

---
+++ T-SQL/14 Data Modifications - DELETE/00 DELETE.sql
@@ -0,0 +1,38 @@
+
+---------------------------------------------------------------------
+-- DML (Data Modification & Retrieval)
+-- DML includes the statements SELECT, INSERT, UPDATE, DELETE, TRUNCATE, and MERGE.
+
+-- DELETE
+
+-- T-SQL provides two statements for deleting rows from a table: DELETE and TRUNCATE
+-- The DELETE statement is a standard statement used to delete data from a table based on an optional filter predicate.
+-- Syntax - DELETE FROM (WHERE)
+-- The DELETE statement tends to be expensive when you delete a large number of rows, mainly because it’s a fully logged operation.
+
+---------------------------------------------------------------------
+
+USE TSQLV4
+GO
+
+DROP TABLE IF EXISTS dbo.Orders
+
+SELECT * FROM dbo.Orders
+
+SELECT *
+INTO dbo.Orders
+FROM Sales.Orders
+
+--1
+DELETE O
+FROM dbo.Orders O
+
+--2
+DELETE FROM dbo.Orders
+WHERE orderdate > '2014-07-30'
+
+--3
+SET NOCOUNT ON
+DELETE FROM dbo.Orders
+WHERE orderdate > '2014-07-30'
+
