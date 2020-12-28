c9dcaa072817e980c6172e9350c58a156eb90dea
Nilesh Mahale <nileshmahale03@gmail.com>
2020-12-28 11:07:54 -0800

    23 Program Obj - Triggers
    DML - AFTER, INSTEAD OF
    DDL

--- T-SQL/23 Program Obj - Triggers/01 DML Triggers.sql
+++ T-SQL/23 Program Obj - Triggers/01 DML Triggers.sql
@@ -9,7 +9,6 @@
 -- Uses: 1. Auditing
 --       2. Enforcing Integrity Rules
 --       3. Enforcing Policies
---       4. Change management
 -- Triggers in SQL Server fire per statement and not per modified row.
 -- In the trigger’s code, you can access pseudo tables called inserted and deleted that contain the rows that were affected by the modification that caused the trigger to fire. 
 
@@ -17,8 +16,46 @@
 --			 a. AFTER or FOR 
 --           b. INSTEAD OF
 --        2. DDL
+
+-- DML (at Object level)
 ---------------------------------------------------------------------
 
+USE TSQLV4
+GO
+
+DROP TABLE IF EXISTS dbo.T1
+
+CREATE TABLE dbo.T1 (
+	col1 INT
+  , col2 VARCHAR(10)
+)
+
+DROP TABLE IF EXISTS dbo.Audit
+
+CREATE TABLE dbo.Audit (
+	audit_key  INT         IDENTITY(1,1) PRIMARY KEY
+  , event_date DATE        DEFAULT GETDATE()
+  , login_name SYSNAME     DEFAULT ORIGINAL_LOGIN()
+  , col1       INT
+  , col2       VARCHAR(10)
+)
+
+SELECT * FROM dbo.T1
+SELECT * FROM dbo.Audit
+
+INSERT INTO dbo.T1 (col1, col2)
+VALUES (1, 'Trigger')
+      ,(2, 'AFTER/FOR')
 
 
+CREATE TRIGGER dbo.TRG_T1_AFTER_INSERT_Audit ON dbo.T1 AFTER INSERT
+AS
+BEGIN
 
+	INSERT INTO dbo.Audit (col1, col2)
+	SELECT col1, col2
+	FROM inserted
+
+END
+
+GO
--- T-SQL/23 Program Obj - Triggers/02 DDL Triggers.sql
+++ T-SQL/23 Program Obj - Triggers/02 DDL Triggers.sql
@@ -9,7 +9,6 @@
 -- Uses: 1. Auditing
 --       2. Enforcing Integrity Rules
 --       3. Enforcing Policies
---       4. Change management
 -- Triggers in SQL Server fire per statement and not per modified row.
 -- In the trigger’s code, you can access pseudo tables called inserted and deleted that contain the rows that were affected by the modification that caused the trigger to fire. 
 
@@ -17,8 +16,32 @@
 --			 a. AFTER or FOR 
 --           b. INSTEAD OF
 --        2. DDL
+
+-- DDL (Database Trigger)
 ---------------------------------------------------------------------
 
+USE TSQLV4
+GO
 
+DROP TABLE IF EXISTS dbo.DDLAudit
 
+CREATE TABLE dbo.DDLAudit (
+	audit_key  INT         IDENTITY(1,1) PRIMARY KEY
+  , event_date DATE        DEFAULT GETDATE()
+  , login_name SYSNAME     DEFAULT ORIGINAL_LOGIN()
+  , event_log  XML
+)
 
+CREATE TRIGGER TRG_DDL_DDLAudit --Cannot specify a schema name as a prefix to the trigger name for database and server level triggers.
+ON DATABASE 
+FOR  CREATE_TABLE
+   , DROP_TABLE
+   , ALTER_TABLE
+AS
+BEGIN
+	INSERT INTO dbo.DDLAudit (event_log)
+	VALUES (EVENTDATA())
+	PRINT 'You must ask your DBA to create, drop or alter tables!'
+END
+
+SELECT * FROM dbo.DDLAudit
